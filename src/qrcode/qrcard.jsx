c93844a2bf21e220c417c8c6ca4a4b7ce165c86e
Andre Gardi <agardi@paypal.com>
2021-09-16 11:07:37 -0300

    Hide the escape path footer if close feedback survey is enabled.

--- src/qrcode/qrcard.jsx
+++ src/qrcode/qrcard.jsx
@@ -107,6 +107,9 @@ function QRCard({
         }
     };
     const content = survey.isEnabled ? surveyElement : frontView;
+    const escapePathFooter = !survey.isEnabled && (
+        <p className="escape-path">Don&apos;t have the app? Pay with <span className="escape-path__link" onClick={ () => handleClick(FUNDING.PAYPAL) }>PayPal</span> or <span className="escape-path__link" onClick={ () => handleClick(FUNDING.CARD) }>Credit/Debit card</span></p>
+    )
 
     return (
         <Fragment>
@@ -134,7 +137,7 @@ function QRCard({
                     onClick={ () => setState(debugging_nextStateMap.get(state)) }
                 >Next State</button>}
             </div>
-            <p className="escape-path">Don&apos;t have the app? Pay with <span className="escape-path__link" onClick={ () => handleClick(FUNDING.PAYPAL) }>PayPal</span> or <span className="escape-path__link" onClick={ () => handleClick(FUNDING.CARD) }>Credit/Debit card</span></p>
+            { escapePathFooter }
         </Fragment>
     );
 }
