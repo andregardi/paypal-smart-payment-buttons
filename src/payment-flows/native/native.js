99a84a35acca9b93c4d4816df7cdbb0ea29ff412
Andre Gardi <agardi@paypal.com>
2021-10-04 14:45:24 -0300

    Fix flow issue

--- src/payment-flows/native/native.js
+++ src/payment-flows/native/native.js
@@ -199,7 +199,7 @@ function initNative({ props, components, config, payment, serviceData } : InitOp
     }
 
     const flow = initFlow({
-        props, serviceData, config, components, payment, fundingSource, clean, sessionUID, buttonSessionID,
+        props, serviceData, config, components, payment, clean, sessionUID, buttonSessionID,
         callbacks: {
             onInit:            onInitCallback,
             onApprove:         onApproveCallback,
